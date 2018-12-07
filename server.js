e1fe343cc5b5cf0057fe95d1b6abc036569e4284
flyc <yayayahahahaooii@gmail.com>
2018-12-07 17:37:00 +0800

    make dbPath as an argument

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-var db = fs.readFileSync('db/data.json');
-    db = JSON.parse(db);
+var dbPath = 'db/data.json',
+    db = fs.readFileSync(dbPath);
+db = JSON.parse(db);
 
 app.get('/list', function(req, res) {
-    console.log(db);
-
     res.json([1, 2, 3, 4, 5, 5, 7, 7, 8, 9, 0, 1]);
 });
 
